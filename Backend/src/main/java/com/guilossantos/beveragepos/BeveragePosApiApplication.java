d15e27feacf893b6f03e8b15199d6c79de68349c
Guilherme Santos <Guir.ls@hotmail.com>
2026-07-15 01:32:06 -0300

    Chore: Projeto Spring Boot Inicializado

---
+++ Backend/src/main/java/com/guilossantos/beveragepos/BeveragePosApiApplication.java
@@ -0,0 +1,13 @@
+package com.guilossantos.beveragepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeveragePosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeveragePosApiApplication.class, args);
+	}
+
+}
